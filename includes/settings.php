d3ae63b35fdace95c389502c5d8ba79f6aa9d6b4
Patrick Lumumba <97313939+lumumbapl@users.noreply.github.com>
2024-07-09 16:02:01 +0300

    Update settings.php

--- includes/settings.php
+++ includes/settings.php
@@ -27,7 +27,7 @@ function qcc_calendar_admin_page() {
 
                     <h2 class="gb-h"><?php _e('Quick Event Calendar Overview', 'quick-event-calendar'); ?></h2>
                     <div class="qcc-video-container">
-                    <script src="https://fast.wistia.com/embed/medias/rh7xzmrk0h.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_rh7xzmrk0h seo=true videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/rh7xzmrk0h/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" aria-hidden="true" onload="this.parentNode.style.opacity=1;" /></div></div></div></div>
+                    <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/981290390?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Quick Event Calendar Settings Overview"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
                     </div>
 
                     <?php
